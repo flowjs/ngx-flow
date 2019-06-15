24c8dc6ecb1c171dd2b891a1528f19a1383adbe7
Martin Nuc <martin@nuc.cz>
2019-06-15 12:40:59 +0200

    minor code improvements

--- projects/ngx-flow/src/lib/ngx-flow.module.ts
+++ projects/ngx-flow/src/lib/ngx-flow.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import * as Flow from '@flowjs/flow.js';
+import Flow from '@flowjs/flow.js';
 import { ButtonDirective } from './button.directive';
 import { DropDirective } from './drop.directive';
 import { FlowInjectionToken } from './flow-injection-token';
 import { FlowDirective } from './flow.directive';
 import { SrcDirective } from './src.directive';
 
+const directives = [ButtonDirective, SrcDirective, DropDirective, FlowDirective];
+
 @NgModule({
   imports: [],
-  declarations: [ButtonDirective, SrcDirective, DropDirective, FlowDirective],
+  declarations: directives,
   providers: [
     {
       provide: FlowInjectionToken,
       useValue: Flow
     }
   ],
-  exports: [ButtonDirective, SrcDirective, DropDirective, FlowDirective]
+  exports: directives
 })
 export class NgxFlowModule {}
